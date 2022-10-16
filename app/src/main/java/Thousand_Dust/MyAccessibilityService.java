c4c487653ab57403eeb8b6bffac9b4aa0c6d7d91
Thousand-Dust <2984613883@qq.com>
2022-10-16 20:59:05 +0800

    新增功能： 1.退出脚本删除绘制 2.使用绘制函数前弹窗提示 修复关闭无障碍再次开启脚本无授权请求

--- app/src/main/java/Thousand_Dust/MyAccessibilityService.java
+++ app/src/main/java/Thousand_Dust/MyAccessibilityService.java
@@ -2,6 +2,7 @@ package Thousand_Dust;
 
 import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
@@ -17,6 +18,9 @@ public class MyAccessibilityService extends AccessibilityService {
     public boolean onUnbind(Intent intent) {
         if (!MyWindowManager.isInstanceEmpty()) {
             MyWindowManager.destInstance();
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            MyWindowManager.newInstance(Tools.getContext());
         }
         return super.onUnbind(intent);
     }
